68b2caa988f779292e611ebec4d3b62d8dc1cf19
mehdi <email2mehdihasan@gmail.com>
2019-11-15 11:16:16 +0100

    added @RefreshScope for live update

--- config-app/src/main/java/me/mh/configapp/RateController.java
+++ config-app/src/main/java/me/mh/configapp/RateController.java
@@ -1,12 +1,14 @@
 package me.mh.configapp;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 3.3 - set up the controller and the html template
 @Controller
+@RefreshScope // 3.4.2 - added refresh scope
 public class RateController {
 	
 	@Value("${rate}")
