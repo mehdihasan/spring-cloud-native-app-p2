765eb3569b54a74b1cb0e8ef3d97af70cce18257
mehdi <email2mehdihasan@gmail.com>
2019-11-15 11:08:49 +0100

    developed the cloud config app

---
+++ config-app/src/main/java/me/mh/configapp/RateController.java
@@ -0,0 +1,32 @@
+package me.mh.configapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 3.3 - set up the controller and the html template
+@Controller
+public class RateController {
+	
+	@Value("${rate}")
+	String rate;
+	
+	@Value("${lanecount}")
+	String laneCount;
+	
+	@Value("${tollstart}")
+	String tollStart;
+	
+	
+	@RequestMapping("/rate")
+	public String getRate(Model model) {
+		
+		model.addAttribute("rateamount", rate);
+		model.addAttribute("lanes", laneCount);
+		model.addAttribute("tollstart", tollStart);
+		
+		return "rateview";
+	}
+
+}
