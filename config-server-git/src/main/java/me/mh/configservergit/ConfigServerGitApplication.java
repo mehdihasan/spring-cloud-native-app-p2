78207bf69a081e063760716163d8900593daba3a
mehdi <email2mehdihasan@gmail.com>
2019-11-21 10:57:45 +0100

    enabled web security along with server side encryption

--- config-server-git/src/main/java/me/mh/configservergit/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/me/mh/configservergit/ConfigServerGitApplication.java
@@ -3,13 +3,23 @@ package me.mh.configservergit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @SpringBootApplication
 @EnableConfigServer // 2.1
-public class ConfigServerGitApplication {
+public class ConfigServerGitApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerGitApplication.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	     http
+	     // ...
+	     .csrf().disable();
+	}
 }
